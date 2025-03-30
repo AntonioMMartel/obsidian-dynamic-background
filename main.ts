b1a57ffa8221aed9d4c6340daf53bac16d060e43
Antonio ML <AntonioMMartel@gmail.com>
2025-03-30 19:21:06 +0100

    Added delete button

--- main.ts
+++ main.ts
@@ -1289,24 +1289,20 @@ class DynamicBackgroundSettingTab extends PluginSettingTab {
 
 
 			addIcon("delete-icon",`<svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>`)
-			/*
-			new Setting(settingItem)
-				.setClass("container-setting-item")
-				//.setName(notePath.notePath)
-				.addButton((button) => {
-					button
-					.setClass("settings-button")
-					.setClass("delete-settings-button")
-					.setIcon("delete-icon")
-					.setTooltip("Remove")
-					.onClick(async () => {
-						new Notice(`Note background deleted`);
-						this.plugin.settings.notesBackgroundMap.remove(notePath);
-						await this.plugin.saveSettings();
-						this.display();
-            		}); 
-				})
-					*/
+
+			const deleteButton = new ButtonComponent(buttonsContainer)
+			deleteButton
+				.setClass("delete-button")
+				.setClass("setting-button")
+				.setIcon("delete-icon")
+				.setTooltip("Remove")
+				.onClick(async () => {
+					new Notice(`Note background settings deleted`);
+					this.plugin.settings.notesBackgroundMap.remove(note);
+					await this.plugin.saveSettings();
+					this.display();
+				}); 
+
 		});
 	}
 }
